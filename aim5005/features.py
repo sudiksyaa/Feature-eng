80206cc3ff968d40513dc320028c443e5fc6c52f
Ian Madhara <iannyfarai@gmail.com>
2025-02-17 09:36:41 -0500

    added standard scaler functionalities and fixed tests

--- aim5005/features.py
+++ aim5005/features.py
@@ -42,4 +42,31 @@ class MinMaxScaler:
 class StandardScaler:
     def __init__(self):
         self.mean = None
-        raise NotImplementedError
+        self.std = None # Standard Deviation
+        
+    def _check_is_array(self, x:np.ndarray) -> np.ndarray:
+        """
+        Try to convert x to a np.ndarray if it'a not a np.ndarray and return. If it can't be cast raise an error
+        """
+        if not isinstance(x, np.ndarray):
+            x = np.array(x)
+            
+        assert isinstance(x, np.ndarray), "Expected the input to be a list"
+        return x
+    
+    def fit(self, x:np.ndarray) -> None:
+        """Compute the mean and standard deviation for standardization"""
+        x = self._check_is_array(x)
+        self.mean=x.mean(axis=0)
+        self.std=x.std(axis=0)
+        
+    def transform(self, x:np.ndarray) -> np.ndarray:
+        """Standardize the given vector
+        """
+        x = self._check_is_array(x)
+        return (x-self.mean)/self.std
+    
+    def fit_transform(self, x:np.ndarray) -> np.ndarray:
+        x = self._check_is_array(x)
+        self.fit(x)
+        return self.transform(x)
